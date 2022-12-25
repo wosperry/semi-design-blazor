811d0ab33f3a22a77a58b857d79cfb9eb80dc6e0
wosperry <55421194+wosperry@users.noreply.github.com>
2022-12-25 12:04:01 +0800

    临时

---
+++ src/Component/BlazorComponent/Components/Tag/STag.razor.cs
@@ -0,0 +1,18 @@
+﻿using BlazorComponent.Components.Tag.Types;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorComponent;
+
+public partial class STag
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public string AvatarSrc { get; set; }
+
+    [Parameter]
+    public AvatarShape AvatarShape { get; set; } = AvatarShape.Circle;
+
+    private const string PrefixCls = "semi-tag";
+}
